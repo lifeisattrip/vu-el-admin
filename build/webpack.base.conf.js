6faf0f17b85f53acf3600f4c8c39a213e1b243fc
kevin <lifeisattrip@gmail.com>
2018-05-15 19:05:23 +0800

    add menu filter

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,8 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      moment: "moment/min/moment-with-locales.min.js"
+      '@': resolve('src')
     }
   },
   module: {
